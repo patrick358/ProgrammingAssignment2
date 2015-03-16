e84993d7f66b412b84308ec15245e6af2617382a
patrick358 <vonmorgenster@gmail.com>
2015-03-16 17:40:19 +0100

    Code
    added the code -> still have to add description

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This code is designed to 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    stored_inverse <- NULL
+    
+    set <- function(y){
+        matrix <<- y
+        stored_inverse <<- NULL
+    }
+    
+    get <- function(){
+        return (matrix)
+    }
+    setinverse <- function (sent_replacement_inverse)
+        stored_inverse <<- sent_replacement_inverse
+    
+    getinverse <-function(){
+        return (stored_inverse)
+    }
+    
+    list(set=set, get=get,
+         setinverse = setinverse
+         getinverse = getinverse)
 }
 
+# Write a short comment describing this function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(madeMatrix, ...) {
+    
+    local_inverse <- madeMatrix$getinverse()
+    
+    if(!is.null(local_inverse)){
+        message("getting cached data")
+    }
+    else {
+        local_data <- madeMatrix$get()
+        local_inverse <- solve(local_data, ...)
+        
+        madeMatrix$setmean(local_inverse)
+        return(local_inverse)
+    }
 }
